9f08f8262a5f37bf09416f67ce74b58cf2f8c9b8
Mathieu Maes <mathieu@zimmo.be>
2018-12-28 16:30:42 +0100

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,16 @@ var webpackConfig = {
                     options: {
                         presets: ['@babel/preset-env']
                     }
-                }
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
             }
         ]
     },
